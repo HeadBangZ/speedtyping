6cb8ba5202f177d4ac9eadb66884a2a73ec96295
Thomas Nordvig Hermansen <thomasnhprivat@gmail.com>
2018-06-14 20:43:59 +0200

    added javascript

---
+++ game.js
@@ -0,0 +1,6 @@
+const originalText = document.querySelector('.original-text p').innerHTML;
+const writtenText = document.querySelector('#written-text');
+const theTimer = document.querySelector('.timer');
+const resetBtn = document.querySelector('#reset-btn');
+const charsPerMinute = document.querySelector('.cpm');
+
