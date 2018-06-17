5825117375a6601891d217a9030204ea0c2e816f
Thomas Nordvig Hermansen <thomasnhprivat@gmail.com>
2018-06-17 21:26:51 +0200

    result calculation

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const writtenText = document.querySelector('#written-text');
 const theTimer = document.querySelector('.timer');
 const resetBtn = document.querySelector('#reset-btn');
 const charsPerMinute = document.querySelector('.cpm');
+const wordsPerMinute = document.querySelector('.wpm');
 
 const timer = [0, 0, 0, 0];
 
@@ -37,6 +38,7 @@ function spellCheck() {
     let textEnteredValue = writtenText.value;
     if (textEnteredValue === originalText) {
         writtenText.style.borderColor = "#00a86b";
+        resultCalculation();
     } else {
         if (originalText.substring(0, textEnteredValue.length) === textEnteredValue) {
             writtenText.style.borderColor = "#189ad3";
@@ -49,7 +51,14 @@ function spellCheck() {
 }
 
 // Result Calculation
+function resultCalculation() {
+    let resultCharactersPerMinute = Math.round(60 / ((timer[0] * 60) + timer[1] + (timer[2] / 100)) * originalText.length);
+    let resultWordsPerMinute = Math.round(60 / ((timer[0] * 60) + timer[1] + (timer[2] / 100)) * originalText.split(" ").length);
 
+    charsPerMinute.innerHTML = "Characters per minute : " + resultCharactersPerMinute;
+    wordsPerMinute.innerHTML = "Words per minute : " + resultWordsPerMinute;
+
+}
 
 // Reset Game
 function reset() {
